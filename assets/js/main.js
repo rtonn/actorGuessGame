0edf0e9e12bf767df662bb086e2c1658879038e5
ercasale97 <ercasale97@gmail.com>
2019-02-12 12:39:32 -0600

    fixed js

--- assets/js/main.js
+++ assets/js/main.js
@@ -114,42 +114,6 @@ container_block.appendChild( block_to_insert );
 
 
 
-
-  
-
-
-
-
-
-    //LIST MOVIES FROM AJAX IN HTML CONTAINER
-
-
-
-     /*$.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-      // Create a new table row element
-      var tRow = $("<tr>");
-
-      // Methods run on jQuery selectors return the selector they we run on
-      // This is why we can create and save a reference to a td in the same statement we update its text
-      var titleTd = $("<td>").text(response.Title);
-      var yearTd = $("<td>").text(response.Year);
-      var actorsTd = $("<td>").text(response.Actors);
-        
-      // Append the newly created table data to the table row
-      tRow.append(titleTd, yearTd, actorsTd);
-      // Append the table row to the table body
-      $("tbody").append(tRow);
-    });*/
-
-
-
-
-
-
-
     //   ^^^^^^   AJAX CALL MOVIES ^^^^^^
     //***************************************
 })
